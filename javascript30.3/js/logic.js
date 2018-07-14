5f3ed40a173429f80d20a4d08929ecd436f081af
David Rowan III <rowand823@gmail.com>
2018-07-13 23:13:41 -0500

    Add files via upload

---
+++ javascript30.3/js/logic.js
@@ -0,0 +1,19 @@
+const secondHand = document.querySelector('.second-hand');
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getMinutes();
+    const hoursDegrees = ((mins / 60) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
